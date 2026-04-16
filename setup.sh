d2df0488e7e8f0c19ade1d3d8d888ee87372d9d3
adeshkumarml <adesh.ks2002@gmail.com>
2026-04-16 20:35:55 +0530

    Setup script updated

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
-!#/usr/bin/env bash
+#!/usr/bin/env bash
 
 echo "Setting up project"
 
-mkdir -p models
-
 echo "Downloading model artifacts"
-
 gdown https://drive.google.com/uc?id=1pjk69tDNgDaj4Z2y-ZBK8rkly8m_2uwm -O models.zip
 
 echo "Unzipping models"
-unzip models.zip -d models/
+unzip models.zip -d .
 rm models.zip
 
 echo "Setup completed!"
